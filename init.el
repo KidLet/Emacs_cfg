8ca0dfab20e4ed29455fa801cda9857649d11b18
KidLet <kidlets@qq.com>
2014-02-02 13:59:34 +0800

    add solarized themem

--- init.el
+++ init.el
@@ -1,11 +1,26 @@
+; color-theme
 (add-to-list 'load-path "~/.emacs.d/plugins/color-theme-6.6.0")
 (add-to-list 'load-path "~/.emacs.d/plugins/color-theme-6.6.0/themes")
 (require 'color-theme)
 (color-theme-initialize)
 (setq color-theme-is-global t)
 
+; Solarized Colorscheme for Emacs
+(add-to-list 'load-path' "~/.emacs.d/plugins/emacs-color-theme-solarized")
+(if
+    (equal 0 (string-match "^24" emacs-version))
+    ;; it's emacs24, so use built-in theme 
+    (require 'solarized-dark-theme)
+  ;; it's NOT emacs24, so use color-theme
+  (progn
+    (require 'color-theme)
+    (color-theme-initialize)
+    (require 'color-theme-solarized)
+    (color-theme-solarized-dark)))
+
 ; auto complete
 (add-to-list 'load-path "~/.emacs.d/plugins/auto-complete-1.3.1/")
 (require 'auto-complete-config)  
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/plugins/auto-complete-1.3.1/ac-dict")  
 (ac-config-default)
+
