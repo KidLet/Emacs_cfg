c6d8d9ecddd994f1cf41fb174d7f1a68711bb79c
KidLet <kidlets@qq.com>
2014-02-16 18:49:57 -0800

    add compile shortcut

--- config/init-keyboard-bind.el
+++ config/init-keyboard-bind.el
@@ -1,11 +1,18 @@
 ; change the key bind for set-mark form C-Space
 (global-set-key (kbd "M-SPC") 'set-mark-command)
 
+; tabbar
 (global-set-key (kbd "M-j") 'tabbar-backward)  
 (global-set-key (kbd "M-k") 'tabbar-forward)
 
-;; PuTTY fix. Ugly. Bad. But it works. (Good)
+; PuTTY fix. Ugly. Bad. But it works. (Good)
 (define-key global-map "\M-[1~" 'beginning-of-line)
 (define-key global-map [select] 'end-of-line)
+
+; compile
+(define-key global-map (kbd "<f5>") 'compile')
+(define-key global-map (kbd "<f6>") 'recompile')
+
+
 
 (provide 'init-keyboard-bind)
